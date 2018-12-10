bda98194da9793b451a857c756cfd2972957eada
Pawel Klosko <PAWEL.KLOSKO@pb.com>
2018-12-10 10:03:05 +0100

    Changes in interfaces

--- tsc/interfaces.js
+++ tsc/interfaces.js
@@ -1,10 +1,30 @@
 "use strict";
+var automobile2 = {
+    brand: "Porsche",
+    speed: 250,
+    speedMethod: function () {
+        console.log("this " + this.brand + " is going at " + this.speed + " miles an hour");
+    }
+};
 var automobile = {
     brand: "BMW",
     speed: 200,
     speedMethod: function () {
-        console.log('this ${this.brand} is going at ${this.speed} miles an hour');
+        console.log("this " + this.brand + " is going at " + this.speed + " miles an hour");
     }
 };
-function car1() {
+function car1(automobile) {
+    automobile.speed = 300;
+    console.log("this " + automobile.brand + " is going at " + automobile.speed + " miles an hour");
 }
+//car1(automobile);
+var AutoMobileClass = /** @class */ (function () {
+    function AutoMobileClass() {
+    }
+    AutoMobileClass.prototype.speedMethod = function (speed) {
+        console.log("My car is going at: " + speed);
+    };
+    return AutoMobileClass;
+}());
+var carObject = new AutoMobileClass();
+carObject.speedMethod(1000);
--- tsc/interfaces.ts
+++ tsc/interfaces.ts
@@ -1,20 +1,59 @@
 interface AutomobileInterface{
 
-    brand: string;
-    speed: number;
-    speedMethod(predkosc: number): void
+    brand?: string;  //adding ? makes the parameter optional
+    speed?: number;
+    speedMethod?(predkosc: number): void
 
 }
+
+interface AutomobileInterface2 extends AutomobileInterface{
+
+    brand: string;
+    speed: number;
+
+}
+
+const automobile2: AutomobileInterface2 ={
+
+
+    brand: "Porsche",
+    speed: 250,
+    speedMethod(){
+    console.log(`this ${this.brand} is going at ${this.speed} miles an hour`);
+    }
+    };
+
+
 
 const automobile: AutomobileInterface ={
     brand: "BMW",
     speed: 200,
     speedMethod(){
-        console.log('this ${this.brand} is going at ${this.speed} miles an hour');
+        console.log(`this ${this.brand} is going at ${this.speed} miles an hour`);
 }
 };
 
-function car1(){
+function car1(automobile: AutomobileInterface){
 
+    automobile.speed = 300;
+
+    console.log(`this ${automobile.brand} is going at ${automobile.speed} miles an hour`);
 
 }
+
+//car1(automobile);
+
+class AutoMobileClass implements AutomobileInterface {
+
+    brand!:string;
+    speed!:number;
+
+    speedMethod(speed:any){
+        console.log(`My car is going at: ${speed}`);
+    }
+
+}
+
+let carObject = new AutoMobileClass();
+
+carObject.speedMethod(1000);
